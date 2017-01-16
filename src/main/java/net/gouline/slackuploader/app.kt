269db9a13dc93cc5a09743971efbd3f498820ad8
mgouline <mgouline@mgouline-mbp.cochlear.com>
2017-01-16 17:49:49 +1100

    Add ProGuard and remove heavy libraries

--- src/main/java/net/gouline/slackuploader/app.kt
+++ src/main/java/net/gouline/slackuploader/app.kt
@@ -1,43 +1,47 @@
 package net.gouline.slackuploader
 
-import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory
-import org.apache.commons.cli.*
-
-private val OPTIONS = Options()
-        .addOption(Option.builder("p")
-                .argName("properties")
-                .desc("path to properties file")
-                .hasArg()
-                .required()
-                .build())
-        .addOption(Option.builder("f")
-                .argName("file")
-                .desc("target file to upload")
-                .hasArg()
-                .required()
-                .build())
-        .addOption(Option.builder("h")
-                .argName("help")
-                .desc("display help")
-                .build())
+import com.beust.jcommander.JCommander
+import com.beust.jcommander.Parameter
+import com.beust.jcommander.ParameterException
+import java.util.*
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    val parser = DefaultParser()
+    val cmd = Commands()
+
+    val commander = JCommander(cmd)
+    commander.setProgramName("slack-uploader")
+
     try {
-        start(parser.parse(OPTIONS, args))
-    } catch (e: ParseException) {
-        println("Error: ${e.message}")
-        HelpFormatter().printHelp("slack-uploader", OPTIONS)
+        commander.parse(*args)
+    } catch (e: ParameterException) {
+        println("ERROR: ${e.message}")
+        exitProcess(1)
     }
 
-    val session = SlackSessionFactory.createWebSocketSlackSession("")
-    session.connect()
-
+    if (cmd.help) {
+        commander.usage()
+    } else {
+        println("Token: ${cmd.token}")
+        println("Title: ${cmd.title}")
+        println("Channels: ${cmd.channels} (${cmd.channels.size})")
+        println("Files: ${cmd.files} (${cmd.files.size})")
+    }
 }
 
-private fun start(line: CommandLine) {
-    if (line.hasOption("h")) {
-        HelpFormatter().printHelp("slack-uploader", OPTIONS)
-        return
-    }
+class Commands {
+    @Parameter(names = arrayOf("--help", "-h"), description = "Displays this usage.", help = true)
+    var help = false
+
+    @field:Parameter(names = arrayOf("--token", "-a"), description = "Authentication token (requires scope 'files:write:user').", required = true)
+    var token: String? = null
+
+    @field:Parameter(names = arrayOf("--title", "-t"), description = "Title of file.", required = true)
+    var title: String? = null
+
+    @field:Parameter(names = arrayOf("--channel", "-c"), description = "Channel names or IDs where the file will be shared.", required = true)
+    var channels: List<String> = ArrayList()
+
+    @field:Parameter(description = "FILE", required = true)
+    var files: List<String> = ArrayList()
 }
