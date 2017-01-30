0b62a6d855c99f9069f5558d8e7ae50dd6528144
mgouline <mgouline@mgouline-mbp.cochlear.com>
2017-01-30 11:37:17 +1100

    Update batch script example

--- scripts/slack-uploader.bat
+++ scripts/slack-uploader.bat
@@ -1,4 +1,5 @@
-set filedate=!date:~10,4!-!date:~6,2!-!date:~4,2!
+@echo off
+set filedate=%date:~10,4%-%date:~7,2%-%date:~4,2%
 java -jar ./build/libs/slack-uploader-shadow-minified.jar \
     -a SLACK_TOKEN \
     -t "Title %filedate%" \
